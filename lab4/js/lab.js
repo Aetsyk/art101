6868e2b15ebb18e2e40ce0b3874d583379e9aaa2
Aetsyk <126929630+Aetsyk@users.noreply.github.com>
2023-10-22 21:23:42 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect 4
+// An implementation of the game
+// Author: Akira Shemansky
+// Date: October 22, 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print a 6x7  slot board
+// Accept user input for each column
+  // Validate user move (if they are able to commit the move)
+  // Move player’s input piece to lowest empty space in the column
+  // Print board with player's move added
+// Evaluate possible moves for computer
+  // Loop through each column to determine the best valid move
+  // Choose computer move
+  // Move computer’s piece to lowest empty space in the column  
+  // Print board with computer's move added 
+// Confirm the winner when 4 pieces make a row, column, or diagonal
